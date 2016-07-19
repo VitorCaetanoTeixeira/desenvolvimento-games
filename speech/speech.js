90368cff048e142483eadc68ec2806589276c2b4
VitorCaetanoTeixeira <vitor130595@gmail.com>
2016-07-19 12:13:22 -0300

    Add files via upload

---
+++ speech/speech.js
@@ -0,0 +1,9 @@
+﻿var speech = new webkitSpeechReconition;
+
+speech.continuous = true;
+speech.interimResults = true;
+speech.lang = 'pt-BR';
+
+speech.onresult = function(e){
+	console.log(e.results[e.result.length-1][0].transcript);
+}
